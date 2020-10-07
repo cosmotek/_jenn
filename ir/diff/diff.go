154becbb9a6aaa3e8e948976d6209a1819abf893
Seth Moeckel <seth.moeckel@gmail.com>
2020-10-07 00:29:44 -0400

    thought about working on the diff engine... not yet :P

--- ir/diff/diff.go
+++ ir/diff/diff.go
@@ -2,7 +2,26 @@ package diff
 
 import "github.com/cosmotek/_jenn/ir"
 
-type Result struct{}
+type ChangeMode int
+
+const (
+	Addition ChangeMode = iota
+	Subtraction
+)
+
+type TypeChange struct {
+	TypeName string
+}
+
+type EnumChange struct {
+	TypeName string
+	ChangeMode
+}
+
+type Result struct {
+	TypeChanges []TypeChange
+	EnumChanges []EnumChange
+}
 
 func Compare(a ir.ModelIR, b ir.ModelIR) (Result, error) {
 	return Result{}, nil
